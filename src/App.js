ff99fa87454a90b14eb84307496d3ad33558553b
elbertsilva <elberteduardomartins@gmail.com>
2021-05-21 15:21:09 -0300

    jogo da velha

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ShoppingList from './main/ShoppingList'
+import Square from './components/Square'
+import Board from './components/Board'
+import Game from "./components/Game"
+
+function App() {
+  return (
+<>
+<Game />
+</>
+  );
+}
+export default App;
+
---
+++ src/main/ShoppingList.jsx
@@ -0,0 +1,17 @@
+import './ShoppingList.css'
+import React from 'react'
+
+export default class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Lista de compras para {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
